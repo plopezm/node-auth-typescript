fd9cba0d6bb12c942776e327653fceaf53f2ee03
Pablo LM <pabloplm@gmail.com>
2018-01-14 12:01:50 +0100

    Updated to version 1.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as mongoose from 'mongoose';
 import { InjectionFactory } from '@plopezm/tsinject';
-import { Server } from "@plopezm/decorated-express";
-//import { Server } from "../../decorated-express/dist/index";
+//import { Server } from "@plopezm/decorated-express";
+import { Server } from "../../decorated-express/dist/index";
 import { UserService } from './services/user.service';
 import { UserResource } from "./resources/user.resource";
+import * as bodyParser from 'body-parser';
+import * as logger from "morgan";
 
 InjectionFactory.register(UserService);
 
@@ -14,5 +16,8 @@ mongoose.connect('mongodb://localhost:27017/auth', { useMongoClient: true})
     });
 
 let server = Server.bootstrap();
+server.config(logger("dev"),
+ bodyParser.json(),
+ bodyParser.urlencoded({extended: true}));
 server.registerResource(UserResource);
 server.start("/api", 8080);
